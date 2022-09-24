a0caf2f4effe504ea141bfddbf4ee8fd41fcd9f9
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2022-09-24 20:43:41 +0530

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}

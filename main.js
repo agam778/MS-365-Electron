4068638074b73b4e9ae184aa2d52ffd520d66c9b
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-02-16 16:37:17 +0530

    Update copyright

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ const menulayout = [
               "https://raw.githubusercontent.com/agam778/MS-365-Electron/main/assets/icon.png",
             product_name: "MS-365-Electron",
             copyright:
-              "Copyright (c) 2021-2022 Agampreet Singh\nOffice, the name, website, images/icons\nare the intellectual properties of Microsoft.",
+              "Copyright (c) 2021-2023 Agampreet Singh\nOffice, the name, website, images/icons\nare the intellectual properties of Microsoft.",
             package_json_dir: __dirname,
             bug_report_url:
               "https://github.com/agam778/MS-365-Electron/issues/",

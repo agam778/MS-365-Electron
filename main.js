5ceccb35101e774108f5c294b95d097e049d4d24
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-03-27 00:49:02 +0530

    Edit menulayout

--- main.js
+++ main.js
@@ -88,24 +88,6 @@ contextMenu({
 });
 
 const menulayout = [
-  ...(process.platform === "darwin"
-    ? [
-        {
-          label: app.name,
-          submenu: [
-            { role: "about" },
-            { type: "separator" },
-            { role: "services" },
-            { type: "separator" },
-            { role: "hide" },
-            { role: "hideothers" },
-            { role: "unhide" },
-            { type: "separator" },
-            { role: "quit" },
-          ],
-        },
-      ]
-    : []),
   {
     label: "Application",
     submenu: [
